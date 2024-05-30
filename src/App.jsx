09708f0707aa6dee45c8a5eb387957cadf47ee42
wind2023storm <super.evil.dev6@outlook.com>
2024-05-30 03:33:42 -0400

    Change build

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import { Canvas } from '@react-three/fiber';
-import { Experience } from './components/Experience';
-import { KeyboardControls, Loader } from '@react-three/drei';
-import { useConvaiClient } from './hooks/useConvaiClient';
-import ChatBubble from './components/chat/Chat';
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+import { KeyboardControls, Loader } from "@react-three/drei";
+import { useConvaiClient } from "./hooks/useConvaiClient";
+import ChatBubble from "./components/chat/Chat";
 
 function App() {
   /**
    * Add apikey and character id here
    */
-  const { client } = useConvaiClient('characterId', 'apikey');
+  const { client } = useConvaiClient(
+    "bfb7635c-1be9-11ef-a588-42010a7be00e",
+    "cfe854dcbd897665986b61e55458a195"
+  );
   return (
     <>
       <KeyboardControls
         map={[
-          { name: 'forward', keys: ['ArrowUp', 'w', 'W'] },
-          { name: 'backward', keys: ['ArrowDown', 's', 'S'] },
-          { name: 'left', keys: ['ArrowLeft', 'a', 'A'] },
-          { name: 'right', keys: ['ArrowRight', 'd', 'D'] },
-          { name: 'sprint', keys: ['Shift'] },
-          { name: 'jump', keys: ['Space'] },
+          { name: "forward", keys: ["ArrowUp", "w", "W"] },
+          { name: "backward", keys: ["ArrowDown", "s", "S"] },
+          { name: "left", keys: ["ArrowLeft", "a", "A"] },
+          { name: "right", keys: ["ArrowRight", "d", "D"] },
+          { name: "sprint", keys: ["Shift"] },
+          { name: "jump", keys: ["Space"] },
         ]}
       >
         <Loader />
